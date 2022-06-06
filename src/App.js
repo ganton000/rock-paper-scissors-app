dd034e24146d76e605b05fd6c7a9c3bce6627e1c
George Anton <SendittoGeorgeAnton@gmail.com>
2022-06-06 17:51:49 -0400

    Created index and App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const App = () => {
+
+	return (
+		<div>
+			⤴
+		</div>
+	)
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+import './index.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+
+root.render(
+	<div>
+		<App />
+	</div>
+)
