52e674e0e214b05cdae38212917398c340aaa7c3
George Anton <SendittoGeorgeAnton@gmail.com>
2022-06-06 18:19:41 -0400

    Added emojis and Play Game button

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+
+const App = () => {
+
+	const [startGame, setStartGame] = useState(true)
+
+	return (
+		<div className="appContainer">
+
+			<button type="submit">Play Game</button>
+
+			{startGame && (
+				<div>
+					<div className="emoji--1">
+						&#128074;
+					</div>
+					<div className="emoji--2">
+						&#9995;
+					</div>
+					<div className="emoji--3">
+						&#9996;
+					</div>
+				</div>
+			)}
+
+		</div>
+	)
+}
+
+export default App;
