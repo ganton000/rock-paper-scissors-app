427ac3749ba8441c07203a5a8caf35afafecba88
George Anton <SendittoGeorgeAnton@gmail.com>
2022-06-08 12:26:27 -0400

    Refactored App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import './App.scss'
 
 const App = () => {
-	const [score, setScore] = useState(0)
+	const [userScore, setUserScore] = useState(0)
 	const [compScore, setCompScore] = useState(0)
 	const [round, setRound] = useState(5);
 	const [startGame, setStartGame] = useState(false);
@@ -12,77 +12,56 @@ const App = () => {
 
 
 	const playGame = (selectedChoice) => {
-	const choicesArr = ['Rock', 'Paper', 'Scissors']
-	const computerChoice = choicesArr[Math.floor(Math.random() * 3)]
-	
-		
-		switch (selectedChoice) {
-			
-			case "Rock":
-				if (computerChoice === 'Scissors') {
-					setScore((prevScore) => prevScore + 1)
-					return 'You win'
-				} else if (computerChoice === 'Rock') {
-					return 'Draw!'
-				} else { 
-					setCompScore((prevScore) => prevScore + 1)
-					return 'You Lose!' }
 
-			case "Paper":
-				if (computerChoice === 'Rock') {
-					setScore((prevScore) => prevScore + 1)
-					return 'You win'
-				} else if (computerChoice === 'Paper') {
-					return 'Draw!'
-				} else { 
-					setCompScore((prevScore) => prevScore + 1)
-					return 'You Lose!'}
+		const choicesArr = ['Rock', 'Paper', 'Scissors']
+		const compChoice = choicesArr[Math.floor(Math.random() * 3)]
 
-			case "Scissors":
-				if (computerChoice === 'Paper') {
-					setScore((prevScore) => prevScore + 1)
-					return 'You win'
-				} else if (computerChoice === 'Scissors') {
-					return 'Draw!'
-				} else { 
-					setCompScore((prevScore) => prevScore + 1)
-					return 'You Lose!' }
-
-			default:
-				return;
+		if (selectedChoice === compChoice) {
+			return 'Draw!'
+		} else if (
+			(selectedChoice === 'rock' && compChoice === 'paper') ||
+			(selectedChoice === 'paper' && compChoice === 'scissors') ||
+			(selectedChoice === 'scissors' && compChoice === 'rock')
+		) {
+			setCompScore(prevScore => prevScore + 1);
+			return 'You Lose!'
+		} else {
+			setUserScore(prevScore => prevScore + 1);
+			return 'You Win!'
 		}
-	}
-	const gameResult = (compScore, score) => {
-					if (score > compScore) {
+	};
+
+	const gameResult = (compScore, userScore) => {
+					if (userScore > compScore) {
 						return 'You Beat the Computer!'
 					} else {
 						return 'You lost to a robot LOSER!'
 					}
-	}
+	};
+
 	useEffect(() => {
-		
-		if (!selectedChoice) { return }
-		const result = playGame(selectedChoice)
+
+		if (!selectedChoice) return;
+
+		const result = playGame(selectedChoice);
 		setResult(result)
-		if (result !== 'Draw!') {
-			setRound((previousState) => {
-				return previousState - 1
-			})
-		}
+		if (result !== 'Draw!') { setRound(prevState => prevState - 1 )};
+
 	}, [selectedChoice])
 
-	if ((score === 3 || compScore === 3) && round !== 0)   {// Sets round count to 0 when either user has 3 wins.
-			setRound(0)
-	}
+	// Sets round count to 0 when either user has 3 wins.
+	if ((userScore === 3 || compScore === 3) && round !== 0)  {
+		setRound(0)
+	};
 
 	return (
-		< div className = "appContainer" >
-			
-		{ !startGame && (<button onClick={() => setStartGame(true)}
+		<div className="appContainer">
+
+		{!startGame && (<button onClick={() => setStartGame(true)}
 				type="submit">Play Game</button>
-			
+
 			)
-}			
+}
 			{startGame && !selectedChoice && (// renders main gameplay
 				<div>
 					<h2>Select a move!</h2>
@@ -90,7 +69,7 @@ const App = () => {
 
 						<img onClick={() => setSelectedChoice("Rock")}
 							className="emoji--1" src={require("../images/rock.png")} alt={"Rock"} />
-	
+
 
 						<img onClick={() => setSelectedChoice("Paper")}
 							className="emoji--2" src={require("../images/paper.png")} alt={"Paper"} />
@@ -98,21 +77,21 @@ const App = () => {
 						<img onClick={() => setSelectedChoice("Scissors")}
 					className="emoji--3" src={require("../images/scissors.png")} alt={"Scissors"} />
 					</div>
-					
+
 				</div>
 			)}
 
 			{startGame && selectedChoice && round !== 0 &&(// Renders for user to choose the next round.
 				<div>
 					<h2>{result}</h2>
-					
+
 					<button onClick={()=>setSelectedChoice(false)}>Next Round</button>
 				</div>
 			) }
 			{startGame && selectedChoice && round === 0 && (// renders the game result
 				<div>
-					<h2>User: {score} Computer: {compScore}</h2>
-					<h2>{gameResult(compScore,score)}</h2>
+					<h2>User: {userScore} Computer: {compScore}</h2>
+					<h2>{gameResult(compScore,userScore)}</h2>
 
 				</div>
 			)}
---
+++ frontend/src/components/Emoji/Emoji.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import './Emoji.scss';
+
+const Emoji = ({ emojiType }) => {
+
+	return (
+		<img
+		className={`emoji--${emojiType}`}
+		src={require(`../../images/${emojiType}.png`)}
+		alt={`{emojiType}`} />
+	)
+}
+
+export default Emoji;
+
