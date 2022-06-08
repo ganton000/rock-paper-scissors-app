60a194a8e02403553a45e6232e145169bc8f6e16
Prince Nwaonicha <lolgfman@gmail.com>
2022-06-07 19:41:17 -0500

    Updated game logic

--- src/components/App.js
+++ src/components/App.js
@@ -3,53 +3,88 @@ import React, { useState, useEffect } from "react";
 import './App.scss'
 
 const App = () => {
-
-	const [startGame, setStartGame] = useState(true);
-
+	const [score, setScore] = useState(0)
+	const [compScore, setCompScore] = useState(0)
+	const [round, setRound] = useState(5);
+	const [startGame, setStartGame] = useState(false);
+	const [result, setResult] = useState(false);
 	const [selectedChoice, setSelectedChoice] = useState(null);
 
-	useEffect(() => {
 
-		if (!selectedChoice) { return }
-
-		const choicesArr = ['Rock', 'Paper', 'Scissors']
-		const computerChoice = choicesArr[Math.floor(Math.random()*3)]
-
+	const playGame = (selectedChoice) => {
+	const choicesArr = ['Rock', 'Paper', 'Scissors']
+	const computerChoice = choicesArr[Math.floor(Math.random() * 3)]
+	
+		
 		switch (selectedChoice) {
-
+			
 			case "Rock":
 				if (computerChoice === 'Scissors') {
+					setScore((prevScore) => prevScore + 1)
 					return "You win"
 				} else if (computerChoice === 'Rock') {
 					return 'Draw!'
-				} else { return "You lose!" }
+				} else { 
+					setCompScore((prevScore) => prevScore + 1)
+					return "You lose!" }
 
 			case "Paper":
 				if (computerChoice === 'Rock') {
+					setScore((prevScore) => prevScore + 1)
 					return "You win"
 				} else if (computerChoice === 'Paper') {
 					return 'Draw!'
-				} else { return "You lose!" }
+				} else { 
+					setCompScore((prevScore) => prevScore + 1)
+					return "You lose!" }
 
 			case "Scissors":
 				if (computerChoice === 'Paper') {
+					setScore((prevScore) => prevScore + 1)
 					return "You win"
 				} else if (computerChoice === 'Scissors') {
 					return 'Draw!'
-				} else { return "You lose!" }
+				} else { 
+					setCompScore((prevScore) => prevScore + 1)
+					return "You lose!" }
 
 			default:
 				return;
 		}
+	}
+	const gameResult = (compScore, userscore) => {
+					if (userscore > compScore) {
+						return "You Beat the Computer!"
+					} else {
+						return "You lost to a robot LOSER!"
+					}
+	}
+	useEffect(() => {
+		
+		if (!selectedChoice) { return }
+		const result = playGame(selectedChoice)
+		setResult(result)
+		if (result !== 'Draw!') {
+			setRound((previousState) => {
+				return previousState - 1
+			})
+		}
+		console.log(score,compScore)
+		
 
 	}, [selectedChoice])
 
 	return (
-		<div className="appContainer">
-
-			<button type="submit">Play Game</button>
-
-			{startGame && (
+		< div className = "appContainer" >
+			
+		{ !startGame && (<button onClick={() => setStartGame(true)}
+				type="submit">Play Game</button>
+			
+			)
+}			
+			{startGame && !selectedChoice && (
+				<div>
+					<h2>Select a move!</h2>
 				<div className="emoji">
 					<div
 					onClick={() => setSelectedChoice("Rock")}
@@ -66,9 +101,25 @@ const App = () => {
 					className="emoji--3">
 						&#9996;
 					</div>
+					</div>
+					
 				</div>
 			)}
 
+			{startGame && selectedChoice && round !== 0 &&(
+				<div>
+					<h2>{result}</h2>
+					
+					<button onClick={()=>setSelectedChoice(null)}>Next Round</button>
+				</div>
+			) }
+			{startGame && selectedChoice && round === 0 && (
+				<div>
+					<h2>User:{score} Computer:{compScore}</h2>
+					<h2>{gameResult()}</h2>
+
+				</div>
+			)}
 		</div>
 	)
 }
