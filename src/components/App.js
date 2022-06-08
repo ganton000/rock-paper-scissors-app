2abc85b6d488b668b22ecb5c9889e80a05a0298f
Prince Nwaonicha <lolgfman@gmail.com>
2022-06-07 23:45:03 -0500

    Updated styling

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
 	const [round, setRound] = useState(5);
 	const [startGame, setStartGame] = useState(false);
 	const [result, setResult] = useState(false);
-	const [selectedChoice, setSelectedChoice] = useState(null);
+	const [selectedChoice, setSelectedChoice] = useState(false);
 
 
 	const playGame = (selectedChoice) => {
@@ -52,8 +52,8 @@ const App = () => {
 				return;
 		}
 	}
-	const gameResult = (compScore, userscore) => {
-					if (userscore > compScore) {
+	const gameResult = (compScore, score) => {
+					if (score > compScore) {
 						return 'You Beat the Computer!'
 					} else {
 						return 'You lost to a robot LOSER!'
@@ -69,10 +69,11 @@ const App = () => {
 				return previousState - 1
 			})
 		}
-		console.log(score,compScore)
-		
-
 	}, [selectedChoice])
+
+	if ((score === 3 || compScore === 3) && round !== 0)   {// Sets round count to 0 when either user has 3 wins.
+			setRound(0)
+	}
 
 	return (
 		< div className = "appContainer" >
@@ -82,41 +83,36 @@ const App = () => {
 			
 			)
 }			
-			{startGame && !selectedChoice && (
+			{startGame && !selectedChoice && (// renders main gameplay
 				<div>
 					<h2>Select a move!</h2>
 				<div className="emoji">
-					<div
-					onClick={() => setSelectedChoice("Rock")}
-					className="emoji--1">
-						&#128074;
-					</div>
-					<div
-					onClick={() => setSelectedChoice("Paper")}
-					className="emoji--2">
-						&#9995;
-					</div>
-					<div
-					onClick={() => setSelectedChoice("Scissors")}
-					className="emoji--3">
-						&#9996;
-					</div>
+
+						<img onClick={() => setSelectedChoice("Rock")}
+							className="emoji--1" src={require("../images/rock.png")} alt={"Rock"} />
+	
+
+						<img onClick={() => setSelectedChoice("Paper")}
+							className="emoji--2" src={require("../images/paper.png")} alt={"Paper"} />
+
+						<img onClick={() => setSelectedChoice("Scissors")}
+					className="emoji--3" src={require("../images/scissors.png")} alt={"Scissors"} />
 					</div>
 					
 				</div>
 			)}
 
-			{startGame && selectedChoice && round !== 0 &&(
+			{startGame && selectedChoice && round !== 0 &&(// Renders for user to choose the next round.
 				<div>
 					<h2>{result}</h2>
 					
-					<button onClick={()=>setSelectedChoice(null)}>Next Round</button>
+					<button onClick={()=>setSelectedChoice(false)}>Next Round</button>
 				</div>
 			) }
-			{startGame && selectedChoice && round === 0 && (
+			{startGame && selectedChoice && round === 0 && (// renders the game result
 				<div>
 					<h2>User: {score} Computer: {compScore}</h2>
-					<h2>{gameResult()}</h2>
+					<h2>{gameResult(compScore,score)}</h2>
 
 				</div>
 			)}
