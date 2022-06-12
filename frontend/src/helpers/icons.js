73a9493df0d29143b9f14b8270d6ee74af63061e
Abel A <abelabrahamx@gmail.com>
2022-06-12 15:43:35 -0500

    fix img name change

--- frontend/src/helpers/icons.js
+++ frontend/src/helpers/icons.js
@@ -1,9 +1,9 @@
 import goBack from '../images/back-button.png'
 import google from '../images/google.png'
 
-import paper from '../images/paper1.png'
-import rock from '../images/rock1.png'
-import scissors from '../images/scissor1.png'
+import paper from '../images/paper.png'
+import rock from '../images/rock.png'
+import scissors from '../images/scissors.png'
 
 const icons = {
   goBack: {
