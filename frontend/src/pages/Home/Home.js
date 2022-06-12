4f8f65d40dbcdf2b7d204c71818a9896b8951efd
Abel A <abelabrahamx@gmail.com>
2022-06-12 11:50:05 -0500

    move style to scss file

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='home'>
       <div className='btn__container'>
-      <Link className='select rock' style={{textDecoration: 'none'}} to='signIn'>
+      <Link className='select rock' to='signIn'>
         <div>
           <img
           src={require('../../images/rock1.png')}
@@ -14,7 +14,7 @@ const Home = () => {
           <h4>Login</h4>
         </div>
         </Link>
-        <Link className='select paper' style={{textDecoration: 'none'}} to='signUp'>
+        <Link className='select paper' to='signUp'>
         <div>
           <img
           src={require('../../images/paper1.png')}
@@ -22,7 +22,7 @@ const Home = () => {
           <h4>Join</h4>
         </div>
         </Link>
-        <Link className='select scissor' style={{textDecoration: 'none'}} to='guest'>
+        <Link className='select scissor' to='guest'>
           <div>
             <img
             src={require('../../images/scissor1.png')}
