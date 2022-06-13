79fc29d155795af9b543046f9aa35513081af92a
Abel A <abelabrahamx@gmail.com>
2022-06-13 12:00:20 -0500

    icons obj helpers -> util move

--- frontend/src/components/GameCard/GameCard.js
+++ frontend/src/components/GameCard/GameCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import icons from '../../helpers/icons';
+import icons from '../../util/icons';
 
 const GameCard = ({ emojiType, result, handleClick }) => {
   return (
