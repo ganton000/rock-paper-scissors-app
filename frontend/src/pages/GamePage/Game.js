9662dd8ae2a8075021a7d6d591eff296db5f30af
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2022-06-12 22:43:40 -0400

    Reformat file using the Prettier plugin

--- frontend/src/pages/GamePage/Game.js
+++ frontend/src/pages/GamePage/Game.js
@@ -1,117 +1,115 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-import Emoji from '../../components/Emoji/Emoji';
-import GameCard from '../../components/GameCard/GameCard';
+import Emoji from "../../components/Emoji/Emoji";
+import GameCard from "../../components/GameCard/GameCard";
 
 const Game = () => {
+  // userName data from guest user
+  const location = useLocation();
+  const { firstName, lastName } = location.state;
 
-	// userName data from guest user 
-	const location = useLocation()
-	const {firstName, lastName} = location.state
+  const [userScore, setUserScore] = useState(0);
+  const [compScore, setCompScore] = useState(0);
+  const [round, setRound] = useState(5);
+  const [startGame, setStartGame] = useState(false);
+  const [result, setResult] = useState(false);
+  const [selectedChoice, setSelectedChoice] = useState(null);
 
-	const [userScore, setUserScore] = useState(0)
-	const [compScore, setCompScore] = useState(0)
-	const [round, setRound] = useState(5);
-	const [startGame, setStartGame] = useState(false);
-	const [result, setResult] = useState(false);
-	const [selectedChoice, setSelectedChoice] = useState(null);
+  const emojisArr = ["rock", "paper", "scissors"];
 
-	const emojisArr = ['rock', 'paper','scissors'];
+  const onHandleClick = (move) => {
+    setSelectedChoice(move);
+  };
 
-	const onHandleClick = (move) => {
-		setSelectedChoice(move);
-	};
+  const playGame = (selectedChoice) => {
+    const compChoice = emojisArr[Math.floor(Math.random() * 3)];
 
-	const playGame = (selectedChoice) => {
+    console.log(compChoice, selectedChoice);
+    console.log(userScore, compScore);
 
-		const compChoice = emojisArr[Math.floor(Math.random() * 3)]
+    if (selectedChoice === compChoice) {
+      return "Draw!";
+    } else if (
+      (selectedChoice === "rock" && compChoice === "paper") ||
+      (selectedChoice === "paper" && compChoice === "scissors") ||
+      (selectedChoice === "scissors" && compChoice === "rock")
+    ) {
+      setCompScore((prevScore) => prevScore + 1);
+      return "You Lose!";
+    } else {
+      setUserScore((prevScore) => prevScore + 1);
+      return "You Win!";
+    }
+  };
 
-		console.log(compChoice, selectedChoice)
-		console.log(userScore, compScore)
+  const gameResult = (compScore, userScore) => {
+    if (userScore > compScore) {
+      return "You Beat the Computer!";
+    } else {
+      return "You lost to a robot LOSER!";
+    }
+  };
 
-		if (selectedChoice === compChoice) {
-			return 'Draw!'
-		} else if (
-			(selectedChoice === 'rock' && compChoice === 'paper') ||
-			(selectedChoice === 'paper' && compChoice === 'scissors') ||
-			(selectedChoice === 'scissors' && compChoice === 'rock')
-		) {
-			setCompScore(prevScore => prevScore + 1);
-			return 'You Lose!'
-		} else {
-			setUserScore(prevScore => prevScore + 1);
-			return 'You Win!'
-		}
-	};
+  useEffect(() => {
+    if (!selectedChoice) return;
 
-	const gameResult = (compScore, userScore) => {
-					if (userScore > compScore) {
-						return 'You Beat the Computer!'
-					} else {
-						return 'You lost to a robot LOSER!'
-					}
-	};
+    const result = playGame(selectedChoice);
+    setResult(result);
+    if (result !== "Draw!") {
+      setRound((prevState) => prevState - 1);
+    }
+  }, [selectedChoice]);
 
-	useEffect(() => {
+  // Sets round count to 0 when either user has 3 wins.
+  if ((userScore === 3 || compScore === 3) && round !== 0) {
+    setRound(0);
+  }
 
-		if (!selectedChoice) return;
+  return (
+    <div className="gameContainer">
+      {!startGame && (
+        <button onClick={() => setStartGame(true)} type="submit">
+          Play Game
+        </button>
+      )}
+      {startGame &&
+        !selectedChoice && ( // renders main gameplay
+          <div>
+            <h2 className="move">Select a move!</h2>
+            <div className="emoji">
+              {emojisArr.map((emoji) => (
+                <GameCard
+                  handleClick={onHandleClick}
+                  emojiType={emoji}
+                  result={emoji}
+                />
+              ))}
+            </div>
+          </div>
+        )}
 
-		const result = playGame(selectedChoice);
-		setResult(result)
-		if (result !== 'Draw!') { setRound(prevState => prevState - 1 )};
-
-	}, [selectedChoice])
-
-	// Sets round count to 0 when either user has 3 wins.
-	if ((userScore === 3 || compScore === 3) && round !== 0)  {
-		setRound(0)
-	};
-
-	return (
-		<div className="gameContainer">
-
-			{!startGame && (
-			<button onClick={() => setStartGame(true)}
-			type="submit">
-				Play Game
-			</button>
-			)}
-			{startGame && !selectedChoice && (// renders main gameplay
-				<div>
-					<h2 className='move'>Select a move!</h2>
-					<div className="emoji">
-						{emojisArr.map(emoji => (
-							<GameCard
-							handleClick={onHandleClick}
-							emojiType={emoji}
-							result={emoji}
-							/>
-						))}
-					</div>
-				</div>
-			)}
-
-			{startGame && selectedChoice && round !== 0 &&(// Renders for user to choose the next round.
-			<div>
-				{/*<h2>{result}</h2>*/}
-				<GameCard
-				emojiType={'rock'}
-				result={result}
-				/>
-				<button onClick={()=>setSelectedChoice(false)}>
-					Next Round
-				</button>
-			</div>
-			) }
-			{startGame && selectedChoice && round === 0 && (// renders the game result
-				<div>
-					<h2>User: {userScore} Computer: {compScore}</h2>
-					<h2>{gameResult(compScore,userScore)}</h2>
-				</div>
-			)}
-		</div>
-	)
+      {startGame &&
+        selectedChoice &&
+        round !== 0 && ( // Renders for user to choose the next round.
+          <div>
+            {/*<h2>{result}</h2>*/}
+            <GameCard emojiType={"rock"} result={result} />
+            <button onClick={() => setSelectedChoice(false)}>Next Round</button>
+          </div>
+        )}
+      {startGame &&
+        selectedChoice &&
+        round === 0 && ( // renders the game result
+          <div>
+            <h2>
+              User: {userScore} Computer: {compScore}
+            </h2>
+            <h2>{gameResult(compScore, userScore)}</h2>
+          </div>
+        )}
+    </div>
+  );
 };
 
 export default Game;
