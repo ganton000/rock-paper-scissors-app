3c2fe1e0fed83cf1cc72195e88f2f143d515db07
Prince Nwaonicha <lolgfman@gmail.com>
2022-06-16 11:48:44 -0500

    Created theme toggle functionality

--- frontend/src/components/BackgroundToggle/BackgroundToggle.js
+++ frontend/src/components/BackgroundToggle/BackgroundToggle.js
@@ -1,39 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+const element = document.querySelector('html');
 
-const handleTheme = (theme) => {
-    switch (theme) {
-        // case 'space':
-        //     <html style={{ backgroundImage: `url("frontend/src/images/light.svg")` }} ></html>
-        case 'ocean':
-            <html style={{ backgroundImage: 'url("frontend/src/images/ocean.svg")' }} ></html>
-            break;
-        // case 'modern':
-        //     <html style={{ backgroundImage: `url("frontend/src/images/light.svg")` }} ></html>
-        // case 'nature':
-        //     <html style={{ backgroundImage: `url("frontend/src/images/light.svg")` }} ></html>
-        // case 'lava':
-        //     <html style={{ backgroundImage: `url("frontend/src/images/light.svg")` }} ></html>
-        // case 'bubbles':
-        //     <html style={{ backgroundImage: `url("frontend/src/images/light.svg")` }} ></html>
-        default:
-            <html style={{ backgroundImage: 'url("frontend/src/images/default.svg")' }} ></html>
-    }
-}
+
 const BackgroundToggle = () => {
     const [theme, setTheme] = useState('default')
 
     const handleClick = () => {
 
-        const newTheme =  setTheme((prevState) => {
-            const themeList = ['default', 'space', 'ocean', 'modern', 'nature', 'lava', 'bubbles']
+        // set theme by passing in a callback function into the hook
+        setTheme((prevState) => {
+            const themeList = ['default', 'ocean', 'geometric', 'mountain', 'bubble']
             const idx = themeList.indexOf(prevState)
-
-            return idx === (themeList.length - 1) ? themeList[0] : themeList[idx + 1]
+            const newState = idx === (themeList.length - 1) ? themeList[0] : themeList[idx + 1]
+            element.classList.replace(prevState,newState)
+            return newState
         })
-        handleTheme(newTheme)
+        
+        
     }
-
-
+    
     return (
         <div><button onClick={handleClick} >{ theme }</button></div>
     )
