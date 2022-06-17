9b25343580b35520a4551bb9c4155adb0d205387
George Anton <SendittoGeorgeAnton@gmail.com>
2022-06-17 17:14:58 -0400

    Minor fix to Background Toggle. Issue still persists

--- frontend/src/components/BackgroundToggle/BackgroundToggle.js
+++ frontend/src/components/BackgroundToggle/BackgroundToggle.js
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
 import icons from '../../utils/icons';
+import { useLocation } from 'react-router-dom';
 
 const htmlElement = document.querySelector('html');
-const backIcon = document.getElementsByTagName('a')[4];
+
 
 const BackgroundToggle = () => {
     const [theme, setTheme] = useState('default')
+    const { pathname } = useLocation();
+
+
+    console.log(pathname);
+
+
+    const changeBackIconOnToggle = (newState) => {
+
+        const backIcon = document.getElementsByTagName('a')[4];
+
+        if (["ocean", "bubble"].includes(newState)) {
+            backIcon.firstChild.src = icons["goBackBlack"].src
+        } else {
+            backIcon.firstChild.src = icons["goBack"].src
+        }
+    }
 
     const handleClick = () => {
 
@@ -15,17 +32,15 @@ const BackgroundToggle = () => {
             const idx = themeList.indexOf(prevState)
             const newState = idx === (themeList.length - 1) ? themeList[0] : themeList[idx + 1]
             htmlElement.classList.replace(prevState,newState)
-            if (["ocean", "bubble"].includes(newState)) {
-                backIcon.firstChild.src = icons["goBackBlack"].src
-            } else {
-                backIcon.firstChild.src = icons["goBack"].src
+            if (['/signup', '/signin', '/guest', '/'].includes(pathname)) {
+                changeBackIconOnToggle(newState);
             }
             return newState
         })
-        
-        
+
+
     }
-    
+
     return (
         <div><button id='Toggle'onClick={handleClick} >{ theme }</button></div>
     )
