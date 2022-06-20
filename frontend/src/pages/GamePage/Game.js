7137e4ac7ca28651bc908d724c892910074f3f80
Frederic Barry <68257842+fredericbarry@users.noreply.github.com>
2022-06-19 21:26:32 -0400

    Reformat using Prettier

--- frontend/src/pages/GamePage/Game.js
+++ frontend/src/pages/GamePage/Game.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-import StartGame from '../../components/StartGame/StartGame';
-import GameCard from '../../components/GameCard/GameCard';
-import PlayGame from '../../components/PlayGame/PlayGame';
-import GameResult from '../../components/GameResult/GameResult';
+import StartGame from "../../components/StartGame/StartGame";
+import GameCard from "../../components/GameCard/GameCard";
+import PlayGame from "../../components/PlayGame/PlayGame";
+import GameResult from "../../components/GameResult/GameResult";
 
 const INITIAL_GAME_RESULT = {
   userScore: 0,
-  compScore: 0
+  compScore: 0,
 };
 
 const Game = () => {
@@ -17,28 +17,25 @@ const Game = () => {
   //const { firstName, lastName } = location.state;
 
   const [gameState, setGameState] = useState(null);
-  const [gameResult, setGameResult] = useState(INITIAL_GAME_RESULT)
+  const [gameResult, setGameResult] = useState(INITIAL_GAME_RESULT);
 
   useEffect(() => {
-
     if (gameResult === INITIAL_GAME_RESULT) return;
 
     setGameState("finished");
   }, [gameResult]);
 
-
   return (
     <div className="game--container">
-      {
-      !gameState ? ( <StartGame handleStart={setGameState} />) :
-      gameState === "play" ? (
-          <PlayGame
+      {!gameState ? (
+        <StartGame handleStart={setGameState} />
+      ) : gameState === "play" ? (
+        <PlayGame
           handleGameState={setGameState}
-          handleGameResult={setGameResult} />
-        ) : (
-          <GameResult
-            gameResult={gameResult}
-            handleEndGame={setGameState} />
+          handleGameResult={setGameResult}
+        />
+      ) : (
+        <GameResult gameResult={gameResult} handleEndGame={setGameState} />
       )}
     </div>
   );
